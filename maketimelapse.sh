41ee13e72262d084bd43469a19b5a0022d504b9d
Kevin Brandon <kevinabrandon@gmail.com>
2020-08-16 23:16:03 -0700

    maketimelapse.sh now renames files itself

--- maketimelapse.sh
+++ maketimelapse.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
+# a script that gets all the images in the daily resized directory,
+# renames them all to be sequential numbers, then encodes a video using
+# ffmpeg. Finally it calls the upload script to upload it to youtube.
 
 OUTDIR="$HOME/sunsetpi/timelapse"
 DAY=$(date +"%Y-%m-%d")
 RESIZEDIR="$OUTDIR/$DAY/resized"
 
 cd $RESIZEDIR
-../../../rename.sh
+
+# rename all the images in the resized directory to be sequential numbers
+a=1
+for i in *.jpg; do
+  new=$(printf "%04d.jpg" "$a")
+  mv -i -- "$i" "$new"
+  let a=a+1
+done
 
 ffmpeg -start_number 1 -i %04d.jpg -c:v libx264 -pix_fmt yuv420p $DAY.mp4
 
-source ../../../../youtube/bin/activate
-
-python upload-video.py \
-   --file $DAY.mp4 \
-   --title "Daily Timelapse $DAY" \
-   --description "Captured, processed, encoded and uploaded automatically from a raspberry pi" \
-   --keywords "Raspberry pi, timelapse, sunset, sunsetpi" \
-   --privacyStatus public \
-   --noauth_local_webserver
-
+$HOME/sunsetpi/upload.sh
