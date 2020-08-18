93f60370bb1533e2396a43632d9cb60683770829
Kevin Brandon <kevinabrandon@gmail.com>
2020-08-17 23:22:28 -0700

    updated where the client_secrets.json is found

--- upload-video.py
+++ upload-video.py
@@ -51,7 +51,8 @@ RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 #   https://developers.google.com/youtube/v3/guides/authentication
 # For more information about the client_secrets.json file format, see:
 #   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-CLIENT_SECRETS_FILE = "client_secrets.json"
+HOME_DIR = os.getenv("HOME")
+CLIENT_SECRETS_FILE = HOME_DIR + "/sunsetpi/client_secrets.json"
 
 # This OAuth 2.0 access scope allows an application to upload files to the
 # authenticated user's YouTube channel, but doesn't allow other types of access.
