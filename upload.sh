eed0bd88bf8a8dd0895cbfeff3e89fa16b2bcffe
Kevin Brandon <kevinabrandon@gmail.com>
2020-08-21 23:33:18 -0700

    video description now includes the sunrise and sunset times

--- upload.sh
+++ upload.sh
@@ -10,12 +10,29 @@ else
   timelapseFile="$SUNSETPI_DATA_NO_MNT/timelapse-mp4s/$DAY.mp4"
 fi
 
+sunriseTime=`$SUNSETPI_PATH/solar-times.py sunrise | \
+  awk {'print $NF'} | \
+  awk -F ':' {'printf "%s:%s", $1, $2'}`
+
+noonTime=`$SUNSETPI_PATH/solar-times.py noon | \
+  awk {'print $NF'} | \
+  awk -F ':' {'printf "%s:%s", $1, $2'}`
+
+sunsetTime=`$SUNSETPI_PATH/solar-times.py sunset | \
+  awk {'print $NF'} | \
+  awk -F ':' {'printf "%s:%s", $1, $2'}`
+
+humanReadableDate=`date +"%A, %B %-d, %Y"`
+
+desc=$(printf "Daily time-lapse for %s.\nCaptured, processed, encoded, and uploaded automatically from a Raspberry Pi 4 in Nipomo, CA.\nSunrise was at %s, solar noon at %s, and sunset at %s.\nThe time-lapse begins 1 hour before sunrise and ends 1 hour after sunset. Photos are taken once per minute until an hour before sunset, then every 15 seconds until the end. The photos are played back at 25fps.\nSee https://github.com/kevinabrandon/sunsetpi for implementation details." \
+  "$humanReadableDate" $sunriseTime $noonTime $sunsetTime)
+
 source $HOME/youtube/bin/activate
 
 python $SUNSETPI_PATH/upload-video.py \
-   --file $timelapseFile \
-   --title "Daily Timelapse $DAY" \
-   --description "Captured, processed, encoded and uploaded automatically from a raspberry pi" \
+   --file "$timelapseFile" \
+   --title "Daily Time-Lapse $DAY" \
+   --description "$desc" \
    --keywords "Raspberry pi, timelapse, sunset, sunsetpi" \
    --privacyStatus public \
    --noauth_local_webserver
