461b50873b263b1f603a172781aed2c0a0a4dfd6
Kevin Brandon <kevinabrandon@gmail.com>
2020-08-22 01:05:52 -0700

    add music info to youtube description

--- upload.sh
+++ upload.sh
@@ -23,9 +23,9 @@ sunsetTime=`$SUNSETPI_PATH/solar-times.py sunset | \
   awk -F ':' {'printf "%s:%s", $1, $2'}`
 
 humanReadableDate=`date +"%A, %B %-d, %Y"`
-
-desc=$(printf "Daily time-lapse for %s.\nCaptured, processed, encoded, and uploaded automatically from a Raspberry Pi 4 in Nipomo, CA.\nSunrise was at %s, solar noon at %s, and sunset at %s.\nThe time-lapse begins 1 hour before sunrise and ends 1 hour after sunset. Photos are taken once per minute until an hour before sunset, then every 15 seconds until the end. The photos are played back at 25fps.\nSee https://github.com/kevinabrandon/sunsetpi for implementation details." \
-  "$humanReadableDate" $sunriseTime $noonTime $sunsetTime)
+musicInfo=`$SUNSETPI_PATH/daily-music.py info`
+desc=$(printf "Daily time-lapse for %s.\nCaptured, processed, encoded, and uploaded automatically from a Raspberry Pi 4 in Nipomo, CA.\nSunrise was at %s, solar noon at %s, and sunset at %s.\nThe time-lapse begins 1 hour before sunrise and ends 1 hour after sunset. Photos are taken once per minute until an hour before sunset, then every 15 seconds until the end. The photos are played back at 25fps.\nMusic: %s from the Youtube Audio Library\nSee https://github.com/kevinabrandon/sunsetpi for implementation details." \
+  "$humanReadableDate" $sunriseTime $noonTime $sunsetTime "$musicInfo")
 
 source $HOME/youtube/bin/activate
 
